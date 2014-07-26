c1b96dd9388e0a939ca6fd93c2ec4778c381db23
Ben Trevor <btrevor@8thlight.com>
2014-07-26 09:16:03 -0500

    Refactor cli spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Git do
+  let(:owner)     { 'owner' }
+  let(:repo_name) { 'repo_name' }
+  let(:file)      { Git::File.new "/#{repo_name}/file/path", owner }
+
   before :each do
     Git::repositories = []
   end
@@ -11,18 +15,18 @@ describe Git do
 
   describe '#init' do
     it 'creates a new repository' do
-      repo = Git::init('project_name', 'owner')
+      repo = Git::init(repo_name, owner)
 
-      expect(Git::repositories.length).to be 1
       expect(repo.class).to be Repository
-      expect(repo.name).to eq 'project_name'
-      expect(repo.owner).to eq 'owner'
+      expect(repo.name).to eq repo_name
+      expect(repo.owner).to eq owner
+      expect(Git::repositories.length).to be 1
     end
   end
 
   describe '#add' do
     it 'starts tracking files' do
-      repo = Git::init('repo_name', 'owner')
+      repo = Git::init(repo_name, owner)
       file = Git::File.new '/repo_name/path', 'content'
 
       expect(repo.working_directory[:untracked].length).to be 1
@@ -39,24 +43,18 @@ describe Git do
 
   describe '#commit' do
     it 'adds a snapshot of files to the repo' do
-      repo = Git::init('repo_name', 'owner')
-      file1 = Git::File.new '/repo_name/path1', 'content1'
-      file2 = Git::File.new '/repo_name/path2', 'content2'
+      repo = Git::init(repo_name, owner)
 
-      Git::add(file1, repo)
-      Git::add(file2, repo)
+      Git::add(file, repo)
       Git::commit(repo)
 
       expect(repo.commits.length).to be 1
     end
 
     it 'clears the staging area' do
-      repo = Git::init('repo_name', 'owner')
-      file1 = Git::File.new '/repo_name/path1', 'content1'
-      file2 = Git::File.new '/repo_name/path2', 'content2'
+      repo = Git::init(repo_name, owner)
 
-      Git::add(file1, repo)
-      Git::add(file2, repo)
+      Git::add(file, repo)
       Git::commit(repo)
 
       expect(repo.working_directory[:tracked][:staged]).to be_empty
